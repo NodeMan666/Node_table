5575fa861361f601c9eb8ba8cac91a19881e7833
NodeMan <adrian.fico@gmx.com>
2017-12-06 12:21:13 -0400

    Update config.default.js

--- config.default.js
+++ config.default.js
@@ -2,21 +2,21 @@
 
 module.exports = {
   twilio: {
-    TWILIO_PHONE_NUMBER: '+14243836835',
-    TWILIO_ACCOUNT_SID: 'AC3adce47ba390351b51d1194f10672be8',
-    TWILIO_AUTH_TOKEN: '4600992c360a5848d81b7962746c55a5'
+    TWILIO_PHONE_NUMBER: '',
+    TWILIO_ACCOUNT_SID: '',
+    TWILIO_AUTH_TOKEN: ''
   },
   stripe: {
-    STRIPE_SECRET: 'sk_test_IzaeCeNIAijqruBm2UoRQ2gX',
-    STRIPE_PUBLISHABLE: 'pk_test_kLq982KtwRPPd4rcn7nrmOb5'
+    STRIPE_SECRET: '',
+    STRIPE_PUBLISHABLE: ''
   },
   redis: {
     REDIS_URL: 'redis://localhost:6379'
   },
 
-  MANDRILL_API_KEY: 'd0kIZ7019O7IpWtU3LeiQw',
-  MONGO_URI: 'mongodb://localhost/localtable-dev',
-  LOCALTABLE_SECRET: 'localtable_secret',
-  SITE_URL: 'http://localtable.co',
+  MANDRILL_API_KEY: '',
+  MONGO_URI: '',
+  LOCALTABLE_SECRET: '',
+  SITE_URL: '',
   NODE_ENV: 'development',
 };
